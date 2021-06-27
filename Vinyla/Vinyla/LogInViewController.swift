7178784f5363748d6f04a29512086f1b546dc4a7
iJoom <ghfdld@naver.com>
2021-06-27 13:52:01 +0900

    create project set login view

---
+++ Vinyla/Vinyla/LogInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LogInViewController.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/06/27.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    let storyBoardID = "LogInViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Vinyla/Vinyla/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/06/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
