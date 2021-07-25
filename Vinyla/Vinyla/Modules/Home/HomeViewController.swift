06bb48e8c3204ca2a11d306d99c430879258fd62
iJoom <ghfdld@naver.com>
2021-07-25 16:16:20 +0900

    add home test blur view and code

--- Vinyla/Vinyla/Modules/Home/HomeViewController.swift
+++ Vinyla/Vinyla/Modules/Home/HomeViewController.swift
@@ -7,23 +7,21 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+final class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var vibrancyImageView: UIImageView!
+    let storyBoardID = "Home"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUI() {
+        vibrancyImageView.layer.cornerRadius = vibrancyImageView.frame.height/2
+        vibrancyImageView.layer.borderWidth = 1
+        vibrancyImageView.layer.borderColor = UIColor.clear.cgColor
     }
-    */
 
 }
