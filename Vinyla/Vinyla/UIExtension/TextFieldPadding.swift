10cf4a8751ddd10fa0c123e97ae055c7e30e3e09
iJoom <ghfdld@naver.com>
2021-07-27 01:21:45 +0900

    [UIExtension] Textfield padding extension code

--- Vinyla/Vinyla/UIExtension/TextFieldPadding.swift
+++ Vinyla/Vinyla/UIExtension/TextFieldPadding.swift
@@ -5,4 +5,13 @@
 //  Created by IJ . on 2021/07/27.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+      }
+}
+
