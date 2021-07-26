cc3dc0b248f8fb1a47e4608bc639dad46bb4fe3f
iJoom <ghfdld@naver.com>
2021-07-27 00:07:55 +0900

    [UI] setting search bar icon color set white

--- Vinyla/Vinyla/Modules/Search/SearchViewController.swift
+++ Vinyla/Vinyla/Modules/Search/SearchViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var vinylSearchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
+    func setUI() {
+        //search bar color custom
+        vinylSearchBar.searchTextField.textColor = UIColor.white
+        if let leftView = vinylSearchBar.searchTextField.leftView as? UIImageView {
+            leftView.image = leftView.image?.withRenderingMode(.alwaysTemplate)
+            leftView.tintColor = UIColor.white
+        }
+        if let rightView = vinylSearchBar.searchTextField.rightView as? UIImageView {
+//            rightView.image = rightView.image?.withRenderingMode(.alwaysTemplate)
+            rightView.tintColor = UIColor.white
+        }
+        //https://fomaios.tistory.com/entry/%EC%84%9C%EC%B9%98%EB%B0%94-%EC%BB%A4%EC%8A%A4%ED%85%80%ED%95%98%EA%B8%B0-Custom-UISearchBar
+        
+    }
 
 }
---
+++ Vinyla/Vinyla/Modules/VinylBox/VinylBoxViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VinylBoxViewController.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/07/26.
+//
+
+import UIKit
+
+class VinylBoxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
