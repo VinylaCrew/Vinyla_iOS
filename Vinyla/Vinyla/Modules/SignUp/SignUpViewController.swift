4730826151d5bc9a15995d966ddecbc345067959
iJoom <ghfdld@naver.com>
2021-07-27 01:21:23 +0900

    [UI] set border background color textfield

--- Vinyla/Vinyla/Modules/SignUp/SignUpViewController.swift
+++ Vinyla/Vinyla/Modules/SignUp/SignUpViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 class SignUpViewController: UIViewController {
 
     let storyBoardID = "SignUp"
+    @IBOutlet weak var nickNameTextField: UITextField!
+    @IBOutlet weak var instagramIDTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        nickNameTextField.delegate = self
+        setUI()
+    }
+    
+    func setUI() {
+        //border
+        nickNameTextField.layer.borderWidth = 1
+        nickNameTextField.layer.cornerRadius = 10
+        nickNameTextField.layer.borderColor = CGColor(red: 40/255, green: 40/255, blue: 41/255, alpha: 1)
+        instagramIDTextField.layer.borderWidth = 1
+        instagramIDTextField.layer.cornerRadius = 10
+        instagramIDTextField.layer.borderColor = CGColor(red: 40/255, green: 40/255, blue: 41/255, alpha: 1)
+        
+        nickNameTextField.attributedPlaceholder = NSAttributedString(string: "6~20자 국문, 영문, 숫자로 입력", attributes: [NSAttributedString.Key.foregroundColor : UIColor(red: 102/255, green: 102/255, blue: 102/255, alpha: 1)])
+        nickNameTextField.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 26/255, alpha: 1)
+        instagramIDTextField.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 26/255, alpha: 1)
+        nickNameTextField.addLeftPadding()
+        instagramIDTextField.addLeftPadding()
+    }
+    
+    @IBAction func touchUpPopButton(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
     }
     
     @IBAction func touchUpLogInButton(_ sender: Any) {
@@ -25,4 +48,28 @@ class SignUpViewController: UIViewController {
         self.navigationController?.pushViewController(nextViewController, animated: true)
     }
     
+}
+
+extension SignUpViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var strings: NSString?
+        
+        if(textField .isEqual(nickNameTextField))
+            {
+            strings=string as NSString;
+            let acceptedChars = NSCharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣㅐㅒㅔㅖㅘㅙㅚㅝㅞㅟㅢ").inverted;
+            if (strings!.rangeOfCharacter(from: acceptedChars.inverted).location != NSNotFound)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+        return true;
+        }
+    }
 }
