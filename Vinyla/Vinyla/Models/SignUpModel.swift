3b4f93e905e3acd493ae06902dff09ebf8021130
iJoom <ghfdld@naver.com>
2021-07-27 01:20:48 +0900

    add signup base model

---
+++ Vinyla/Vinyla/Models/SignUpModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpModel.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/07/27.
+//
+
+import Foundation
+
+struct SignUpModel {
+    let nickName : String?
+    let instagramID : String?
+}
