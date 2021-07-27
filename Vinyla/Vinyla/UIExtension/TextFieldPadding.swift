0f5bcc724df3a6e9296d0090cc7f2b898c437341
iJoom <ghfdld@naver.com>
2021-07-28 01:08:05 +0900

    [UIExtension] UIButton bgcolor extension

--- Vinyla/Vinyla/UIExtension/TextFieldPadding.swift
+++ Vinyla/Vinyla/UIExtension/TextFieldPadding.swift
@@ -15,3 +15,15 @@ extension UITextField {
       }
 }
 
+extension UIButton {
+
+  func setBackgroundColor(_ color: UIColor, for forState: UIControl.State) {
+    UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+    UIGraphicsGetCurrentContext()!.setFillColor(color.cgColor)
+    UIGraphicsGetCurrentContext()!.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+    let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    self.setBackgroundImage(colorImage, for: forState)
+  }
+
+}
