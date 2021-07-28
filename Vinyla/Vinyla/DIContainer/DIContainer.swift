bab7626c01ba6bea716bf1c5dd385f1ff9ae2a4e
iJoom <ghfdld@naver.com>
2021-07-29 00:03:27 +0900

    [system] add base DIContainer

--- Vinyla/Vinyla/DIContainer/DIContainer.swift
+++ Vinyla/Vinyla/DIContainer/DIContainer.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+final class DIContainer {
+    private var storage: [String: AnyObject] = [:]
+    
+    static let shared = DIContainer()
+    private init() {}
+    //현재 DIContainer는 ViewModel이 1:1 대응이라 필요없지만, 추후 네트워크 객체를 DIContainer 활용하면 될듯
+    
+    func register(_ object: AnyObject) {
+        let key = String(reflecting: type(of: object))
+        storage[key] = object
+    }
+    
+    func resolve<T>(_ type: T.Type) -> T? {
+        let key = String(reflecting: type)
+        guard let object = storage[key] as? T else { return nil }
+        return object
+    }
+}
