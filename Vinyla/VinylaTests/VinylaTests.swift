b76863714bf0d1d43c819e89e79078cc788e1b8e
iJoom <ghfdld@naver.com>
2021-07-29 00:02:49 +0900

    add project contents

---
+++ Vinyla/VinylaTests/VinylaTests.swift
@@ -0,0 +1,39 @@
+//
+//  VinylaTests.swift
+//  VinylaTests
+//
+//  Created by IJ . on 2021/07/28.
+//
+
+import XCTest
+@testable import Vinyla
+
+class VinylaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+//        let view = SignUpViewController()
+//        view.setViewModel(MockSignUpViewModel())
+//
+//        XCTAssert(view.)
+        
+        
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
