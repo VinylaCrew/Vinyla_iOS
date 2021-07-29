7b593348f249cee9f10c8c8eaea3c3060ca90697
iJoom <ghfdld@naver.com>
2021-07-30 01:46:58 +0900

    [UI] add search table view cell layout UI

--- Vinyla/Vinyla/Modules/Search/SearchTableViewCell.swift
+++ Vinyla/Vinyla/Modules/Search/SearchTableViewCell.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class SearchTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var searchVinylImageView: UIImageView!
+    @IBOutlet weak var songTitleLabel: UILabel!
+    @IBOutlet weak var singerNameLabel: UILabel!
+    @IBOutlet weak var customSeperatorView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        searchVinylImageView.layer.cornerRadius = searchVinylImageView.frame.height/2
+        
+        customSeperatorView.backgroundColor = UIColor(red: 40/255, green: 40/255, blue: 41/255, alpha: 1)
+//        UIColor(red: 40/255, green: 40/255, blue: 41/255, alpha: 1)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
