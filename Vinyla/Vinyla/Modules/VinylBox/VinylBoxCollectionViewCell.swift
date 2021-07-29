fe488a6ffeb837a952c9644e7ffa6e1a50bafc28
iJoom <ghfdld@naver.com>
2021-07-30 01:45:56 +0900

    [feat] create custom vinyl box cell

---
+++ Vinyla/Vinyla/Modules/VinylBox/VinylBoxCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  VinylBoxCollectionViewCell.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/07/30.
+//
+
+import UIKit
+
+class VinylBoxCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var vinylBoxImageView: UIImageView!
+    let identifier = "VinylBoxCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
