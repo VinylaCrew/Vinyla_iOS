e2373137f0fdde82b0bb72ae0c1569911ac2f0ce
iJoom <ghfdld@naver.com>
2021-07-31 17:53:52 +0900

    [feat] create example rxswift, rxcocoa tableview design (no delegate datasource)

--- Vinyla/Vinyla/Modules/Search/SearchViewController.swift
+++ Vinyla/Vinyla/Modules/Search/SearchViewController.swift
@@ -6,18 +6,24 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var vinylSearchBar: UISearchBar!
     @IBOutlet weak var searchTableView: UITableView!
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        searchTableView.dataSource = self
+//        searchTableView.dataSource = self
+        
         setUI()
-        setTableViewCellXib()
+        setTableViewCellXib() //rxcocoa도 그대로 사용
+        bindTableView()
+        didSelectCell()
     }
     
     func setUI() {
@@ -33,12 +39,45 @@ class SearchViewController: UIViewController {
         }
         //https://fomaios.tistory.com/entry/%EC%84%9C%EC%B9%98%EB%B0%94-%EC%BB%A4%EC%8A%A4%ED%85%80%ED%95%98%EA%B8%B0-Custom-UISearchBar
         
+        
     }
     
     func setTableViewCellXib() {
         let searchNib = UINib(nibName: "SearchTableViewCell", bundle: nil)
         searchTableView.register(searchNib, forCellReuseIdentifier: "searchTableViewCell")
     }
+    
+    @IBAction func touchUpViewPopButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func bindTableView() {
+        let cities = Observable.of(["Lisbon", "Copenhagen", "London", "Madrid", "Vienna", "Seoul"])
+        cities.bind(to: searchTableView.rx.items) { (tableView: UITableView, index: Int, element: String) in
+        
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "searchTableViewCell") as? SearchTableViewCell else { return UITableViewCell()}
+                        
+            cell.songTitleLabel.text = element
+            
+            return cell
+        }.disposed(by: disposeBag)
+        
+    }
+    
+    func didSelectCell() {
+        searchTableView.rx.modelSelected(String.self)
+            .subscribe(onNext: { model in
+                print("seletecd \(model)")
+            })
+            .disposed(by: disposeBag)
+        
+        searchTableView.rx.itemSelected
+            .subscribe(onNext: { indexPath in
+                print("selected \(indexPath)")
+            })
+            .disposed(by: disposeBag)
+    }
+    
 }
 
 
