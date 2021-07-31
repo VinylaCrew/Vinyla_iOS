b7afc6fea1e4da0eff489589335d2bf166fbf108
iJoom <ghfdld@naver.com>
2021-07-31 17:52:14 +0900

    [system] create base coordinator 자동로그인 및 화면전환

---
+++ Vinyla/Vinyla/AppCoordinator.swift
@@ -0,0 +1,20 @@
+//
+//  AppCoordinator.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/07/31.
+//
+
+import UIKit
+
+class AppCoordinator {
+    
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    
+    
+}
