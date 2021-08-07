8ad0d93c995adf5b0859b3e47ec50d6f0fd417ec
iJoom <ghfdld@naver.com>
2021-08-07 19:27:09 +0900

    edit code

--- Vinyla/Vinyla/AppDelegate.swift
+++ Vinyla/Vinyla/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         DIContainer.shared.register(SignUpViewModel())
         UIApplication.shared.statusBarStyle = .lightContent
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first ?? "Not Found")
         return true
     }
 
@@ -42,7 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
              application to it. This property is optional since there are legitimate
              error conditions that could cause the creation of the store to fail.
             */
-            let container = NSPersistentContainer(name: "DataModel")
+            let container = NSPersistentContainer(name: "MainFavoriteModel")
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
                     // Replace this implementation with code to handle the error appropriately.
