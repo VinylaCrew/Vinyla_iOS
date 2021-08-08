21a913072f9705c3ec16c1567e09ce20c351afba
iJoom <ghfdld@naver.com>
2021-08-08 22:04:51 +0900

    add view item in addinformationview

--- Vinyla/Vinyla/Modules/AddInformation/AddInformationViewController.swift
+++ Vinyla/Vinyla/Modules/AddInformation/AddInformationViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class AddInformationViewController: UIViewController {
 
+    @IBOutlet weak var homeMainFavoriteButton: UIButton!
+    @IBOutlet weak var addInformationImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func touchUpHomeFavoriteButton(_ sender: Any) {
+        //home 대표 이미지 변경
+        //vinly box 체크버튼 표시되고, 정렬 되는 로직
+        //isFavorite = true
     }
-    */
-
+    
 }
