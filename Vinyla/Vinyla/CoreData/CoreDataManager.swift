d1a1cedf2c01682915a7942b99b6fa34092f5938
iJoom <ghfdld@naver.com>
2021-08-08 22:04:23 +0900

    add fetch item coredata manager

--- Vinyla/Vinyla/CoreData/CoreDataManager.swift
+++ Vinyla/Vinyla/CoreData/CoreDataManager.swift
@@ -15,6 +15,19 @@ class CoreDataManager {
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    func saveVinylBox(songTitle: String, singer: String, vinylImage: Data) {
+        let vinylBoxInstance = VinylBox(context: context)
+        vinylBoxInstance.signer = singer
+        vinylBoxInstance.songTitle = songTitle
+        vinylBoxInstance.vinylImage = vinylImage
+        do {
+            try context.save()
+            print("Vinyl Box is saved")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
     func saveImage(data: Data) {
         
         let imageInstance = MyImage(context: context)
@@ -26,6 +39,18 @@ class CoreDataManager {
         } catch {
             print(error.localizedDescription)
         }
+    }
+    func fetchVinylBox() -> [VinylBox] {
+        var fetchVinylBox = [VinylBox]()
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "VinylBox")
+        
+        do {
+            fetchVinylBox = try context.fetch(fetchRequest) as! [VinylBox]
+        } catch {
+            print("Error while fetching the image")
+        }
+        
+        return fetchVinylBox
     }
     
     func fetchImage() -> [MyImage] {
@@ -42,6 +67,7 @@ class CoreDataManager {
     }
     
     func delete(imageID: String) {
+        print("delete 호출")
         let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "MyImage")
         fetchRequest.predicate = NSPredicate(format: "imageID = %@", imageID)
         
@@ -51,27 +77,46 @@ class CoreDataManager {
             for object in results {
                 context.delete(object)
             }
-
-            print(results.count)
+            
+            print("CoreData object 수:\(results.count)")
             // Or delete first object: , 처음 1번만 오브젝트삭제됨
-//            if results.count > 0 {
-//                context.delete(results[0])
-//            }
+            //            if results.count > 0 {
+            //                context.delete(results[0])
+            //            }
+            for object in results {
+                print("oject \(object)")
+            }
+            try context.save() // data 추가 삭제후 필수로
+            
         } catch {
-            print("Error while delete")
+            print("Error while delete func")
+            print(error.localizedDescription)
         }
-         
+        
     }
-
+    
     
     
     func printData() {
-            do { let myImage = try context.fetch(MyImage.fetchRequest()) as! [MyImage]
-                myImage.forEach { print($0.favoriteImage)
-                    print($0.imageID)
-                    print("Data 출력") } }
-            catch { print(error.localizedDescription)
-                print("에러뤄")
-            }
+        do { let myImage = try context.fetch(MyImage.fetchRequest()) as! [MyImage]
+            print("Data 출력")
+            myImage.forEach { print($0.favoriteImage)
+                print($0.imageID)
+            } }
+        catch { print(error.localizedDescription)
+            print("에러뤄")
+        }
+    }
+    
+    func printVinylBoxData() {
+        do { let vinylBox = try context.fetch(VinylBox.fetchRequest()) as! [VinylBox]
+            print("Data 출력")
+            vinylBox.forEach { print($0.vinylImage)
+                print($0.songTitle)
+                print($0.signer)
+            } }
+        catch { print(error.localizedDescription)
+            print("에러뤄")
+        }
     }
 }
