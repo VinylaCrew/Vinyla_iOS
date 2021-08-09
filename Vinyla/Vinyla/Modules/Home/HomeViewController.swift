418ffc73bb37279c32ac0c19a70072bb79caf90b
iJoom <ghfdld@naver.com>
2021-08-09 12:36:15 +0900

    [feat] homeview test code

--- Vinyla/Vinyla/Modules/Home/HomeViewController.swift
+++ Vinyla/Vinyla/Modules/Home/HomeViewController.swift
@@ -11,6 +11,9 @@ final class HomeViewController: UIViewController {
     
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var vibrancyImageView: UIImageView!
+    @IBOutlet weak var homeImageView: UIImageView!
+    @IBOutlet weak var testImageView: UIImageView!
+    
     let storyBoardID = "Home"
     
     private weak var coordiNator: AppCoordinator?
@@ -29,10 +32,20 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-        
+//        if let imageData = testImageView.image?.jpegData(compressionQuality: 1) {
+//            CoreDataManager.shared.saveImage(data: imageData)
+//        }
+        //이미지 2개 추가 하나는 username = nil
+        CoreDataManager.shared.delete(imageID: "name1")
         
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        let testCoreImage = CoreDataManager.shared.fetchImage()
+//        self.homeImageView.image = UIImage(data: testCoreImage[0].favoriteImage!)
+        CoreDataManager.shared.printData()
+        
+    }
     func setUI() {
 //        vibrancyImageView.layer.cornerRadius = vibrancyImageView.frame.height/2
 //        vibrancyImageView.layer.borderWidth = 1
