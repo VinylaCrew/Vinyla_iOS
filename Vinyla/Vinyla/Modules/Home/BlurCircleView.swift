733b32ed45473408b5baafef9736c41869ec21ce
iJoom <ghfdld@naver.com>
2021-08-09 12:35:48 +0900

    [UI] create recycle blur view

---
+++ Vinyla/Vinyla/Modules/Home/BlurCircleView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurCircleView.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/09.
+//
+
+import UIKit
+
+class BlurCircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
