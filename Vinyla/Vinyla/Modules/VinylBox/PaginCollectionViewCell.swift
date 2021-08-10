373285a162551cbafec37b4961cee54d56a58e43
iJoom <ghfdld@naver.com>
2021-08-10 21:48:40 +0900

    [UI] add paging collectionview in vinylboxview

---
+++ Vinyla/Vinyla/Modules/VinylBox/PaginCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PaginCollectionViewCell.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/10.
+//
+
+import UIKit
+
+class PaginCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
