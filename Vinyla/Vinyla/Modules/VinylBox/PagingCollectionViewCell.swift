7ac935c0f3daac38a1c428cf7553ed468caf2692
iJoom <ghfdld@naver.com>
2021-08-11 23:07:40 +0900

    [bug] fixed error cell name

---
+++ Vinyla/Vinyla/Modules/VinylBox/PagingCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  PaginCollectionViewCell.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/10.
+//
+
+import UIKit
+
+class PaginCollectionViewCell: UICollectionViewCell {
+    
+
+    let cellIdentifier = "pagingCell"
+    @IBOutlet weak var vinylBoxCollectionView: UICollectionView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        vinylBoxCollectionView.delegate = self
+        vinylBoxCollectionView.dataSource = self
+        let vinylBoxCellNib = UINib(nibName: "VinylBoxCollectionViewCell", bundle: nil)
+        vinylBoxCollectionView.register(vinylBoxCellNib, forCellWithReuseIdentifier: "VinylBoxCell")
+        
+        print("Paging")
+        print(UIScreen.main.bounds.size.height)
+    }
+
+}
+
+extension PaginCollectionViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "VinylBoxCell", for: indexPath) as? VinylBoxCollectionViewCell else { return UICollectionViewCell() }
+//                cell.vinylBoxImageView.image = downScaleImage(imageData: vinylBoxes[indexPath.row].vinylImage!, for: CGSize(width: 200, height: 200), scale: 0.7)
+//                cell.signerLabel.text = vinylBoxes[indexPath.row].signer
+//                cell.songTitleLabel.text = vinylBoxes[indexPath.row].songTitle
+                return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        22
+    }
+    
+}
