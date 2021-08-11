45bc7eee861b27321ee1e56b4872495062ba9971
iJoom <ghfdld@naver.com>
2021-08-11 23:00:31 +0900

    [UI] create home recent collection view

---
+++ Vinyla/Vinyla/Modules/Home/RecentVinylCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecentVinylCollectionViewCell.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/11.
+//
+
+import UIKit
+
+class RecentVinylCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
