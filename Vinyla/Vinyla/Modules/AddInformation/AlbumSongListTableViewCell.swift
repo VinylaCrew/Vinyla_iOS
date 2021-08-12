b457a33f10cafac36ea79290150fd4a53f5f8e6b
iJoom <ghfdld@naver.com>
2021-08-12 11:23:30 +0900

    [UI] add Album list tableview cell

---
+++ Vinyla/Vinyla/Modules/AddInformation/AlbumSongListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlbumSongListTableViewCell.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/12.
+//
+
+import UIKit
+
+class AlbumSongListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
