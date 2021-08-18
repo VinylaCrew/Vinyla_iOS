eec2b4953050d3a093baec9f2f63c6af8955a31f
iJoom <ghfdld@naver.com>
2021-08-18 16:57:30 +0900

    add delete data logic viewmodel code

--- Vinyla/Vinyla/Modules/DeleteInformation/DeleteInformationViewModel.swift
+++ Vinyla/Vinyla/Modules/DeleteInformation/DeleteInformationViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import RxSwift
+
+final class DeleteInformationViewModel {
+    var songTitle: String?
+//    var songObservalble: Observable<String?>
+
+    func deleteVinylBoxData() {
+        print(self.songTitle)
+        guard let deleteSong = songTitle else { return }
+        CoreDataManager.shared.deleteSpecificVinylBox(songTitle: deleteSong)
+        print("completed deleteVinylBoxData")
+    }
+}
