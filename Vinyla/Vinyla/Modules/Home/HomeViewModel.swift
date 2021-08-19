eca7e7e7b80746677153980600e7a60ae5d01ce5
iJoom <ghfdld@naver.com>
2021-08-19 21:19:21 +0900

    add home viewmodel , vinyl count logic level gague

--- Vinyla/Vinyla/Modules/Home/HomeViewModel.swift
+++ Vinyla/Vinyla/Modules/Home/HomeViewModel.swift
@@ -9,12 +9,44 @@ import Foundation
 
 class HomeViewModel {
     var homeStirng: String?
+    private(set) var recentVinylBoxData: [VinylBox]?
     init() {
-        self.homeStirng = "test"
-        print("init homeviewmodel",self.homeStirng)
+        
     }
-
+    
     deinit {
         print("deinit HomeViewModel")
+    }
+    
+    func fetchRecentVinylData() {
+        self.recentVinylBoxData = CoreDataManager.shared.fetchRecentVinylBox()
+    }
+    
+    func getRecentVinylBoxData(indexPathRow: Int) -> Data? {
+        if let check = self.recentVinylBoxData, indexPathRow < check.count {
+            return check[indexPathRow].vinylImage
+        }else {
+            return nil
+        }
+    }
+    func getTotalVinylBoxCount() -> Int {
+        guard let totalVinylCount = CoreDataManager.shared.getCountVinylBoxData() else { return 0 }
+        return totalVinylCount
+    }
+    
+    func getLevelGague() -> String {
+        let totalVinylCount = self.getTotalVinylBoxCount()
+        switch totalVinylCount {
+        case 0:
+            return "\(totalVinylCount)/1"
+        case 1...9:
+            return "\(totalVinylCount)/9"
+        case 10...49:
+            return "\(totalVinylCount)/49"
+        case 50...499:
+            return "\(totalVinylCount)/499"
+        default:
+            return "\(totalVinylCount)/"
+        }
     }
 }
