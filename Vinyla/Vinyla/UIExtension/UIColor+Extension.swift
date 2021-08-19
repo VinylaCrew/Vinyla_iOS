f1c0e72686269f32063ee9aea24514b71e3b454a
iJoom <ghfdld@naver.com>
2021-08-20 02:11:00 +0900

    add color extension

--- Vinyla/Vinyla/UIExtension/UIColor+Extension.swift
+++ Vinyla/Vinyla/UIExtension/UIColor+Extension.swift
@@ -5,4 +5,16 @@
 //  Created by IJ . on 2021/08/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    class func buttonDisabledColor() -> UIColor {
+        return UIColor(red: 35/255, green: 35/255, blue: 36/255, alpha: 1)
+    }
+    class func vinylaMainOrangeColor() -> UIColor {
+        return UIColor(red: 255/255, green: 80/255, blue: 0/255, alpha: 1)
+    }
+    static func buttonDisabledTextColor() -> UIColor {
+        return UIColor(red: 170/255, green: 170/255, blue: 170/255, alpha: 1)
+    }
+}
