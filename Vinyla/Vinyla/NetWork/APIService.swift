79abadaf71ac60f8d2eb7e353b20581966a7bb09
iJoom <ghfdld@naver.com>
2021-08-27 14:52:56 +0900

    [NetWork] add base api service with rx moya

--- Vinyla/Vinyla/NetWork/APIService.swift
+++ Vinyla/Vinyla/NetWork/APIService.swift
@@ -5,4 +5,36 @@
 //  Created by IJ . on 2021/08/27.
 //
 
-import Foundation
+import Moya
+import RxSwift
+
+protocol VinylAPIServiceProtocol {
+    func searchVinyl(vinylName: String) -> Observable<[SearchModel]>
+}
+
+final class VinylAPIService: VinylAPIServiceProtocol {
+
+    private let provider: MoyaProvider<APITarget>
+
+    init(provider: MoyaProvider<APITarget> = MoyaProvider<APITarget>()) {
+        self.provider = provider
+    }
+
+    func searchVinyl(vinylName: String) -> Observable<[SearchModel]> {
+        return Observable.create() { [weak self] emitter in
+            self?.provider.request(.vinylSearch(urlParameters: vinylName)) { result in
+                switch result {
+                case .success(let response):
+                    let decodedData = try? JSONDecoder().decode([SearchModel].self, from: response.data)
+                    emitter.onNext(decodedData ?? [])
+                case .failure(let error):
+                    emitter.onError(error)
+                }
+                emitter.onCompleted()
+            }
+            return Disposables.create()
+        }
+
+    }
+
+}
--- Vinyla/Vinyla/NetWork/APITarget.swift
+++ Vinyla/Vinyla/NetWork/APITarget.swift
@@ -5,4 +5,49 @@
 //  Created by IJ . on 2021/08/27.
 //
 
-import Foundation
+import Moya
+
+enum APITarget: TargetType {
+    case vinylSearch(urlParameters: String?)
+
+    var baseURL: URL {
+        return URL(string:"http://")!
+    }
+
+    var path: String {
+        switch self {
+        case .vinylSearch:
+            return "/vinyls/search"
+        }
+    }
+
+    var method: Method {
+        switch self {
+        case .vinylSearch:
+            return .get
+        }
+    }
+
+    var sampleData: Data {
+        return .init()
+    }
+
+    var task: Task {
+        switch self {
+        case let .vinylSearch(urlParameters):
+            if let vinylName = urlParameters {
+                return .requestParameters(parameters: ["q" : vinylName], encoding: URLEncoding.default)
+            }
+            print("User Nick Name Error")
+            return .requestParameters(parameters: ["q" : "error"], encoding: URLEncoding.default)
+
+        }
+    }
+
+    var headers: [String : String]? {
+        switch self {
+        
+        default: return ["Content-Type": "application/json"]
+        }
+    }
+}
