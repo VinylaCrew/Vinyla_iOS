3afcc1a110fd0c5b546404734782a191b5a66710
iJoom <ghfdld@naver.com>
2021-08-30 00:31:14 +0900

    테이블뷰 터치시 gradient layer가 추가로 insert 되지 않도록 로직추가

--- Vinyla/Vinyla/Modules/LevelDesign/LevelDesignTableViewCell.swift
+++ Vinyla/Vinyla/Modules/LevelDesign/LevelDesignTableViewCell.swift
@@ -13,15 +13,23 @@ class LevelDesignTableViewCell: UITableViewCell {
     @IBOutlet weak var levelLabel: UILabel!
     @IBOutlet weak var levelInformationLabel: UILabel!
     @IBOutlet weak var levelMentLabel: UILabel!
-
+    var isGradient: Bool?
+    var isOnce: Int = 0
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
 
     }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if isGradient == true && isOnce == 0{
+            self.contentView.setGradient(color1: UIColor.gradientStartColor(), color2: UIColor.gradientEndColor())
+            isOnce += 1
+        }
+    }
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//    }
 
 }
