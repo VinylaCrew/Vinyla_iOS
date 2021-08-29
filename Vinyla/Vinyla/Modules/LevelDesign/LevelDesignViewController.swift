cc4d05e076104ae400eda7169b6e4ccff02daeef
iJoom <ghfdld@naver.com>
2021-08-29 20:19:46 +0900

    [UI] set UI Level Design View

---
+++ Vinyla/Vinyla/Modules/LevelDesign/LevelDesignViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LevelDesignViewController.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/29.
+//
+
+import UIKit
+
+class LevelDesignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Vinyla/Vinyla/Modules/LevelDesign/LevelDesignViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LevelDesignViewModel.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/29.
+//
+
+import Foundation
