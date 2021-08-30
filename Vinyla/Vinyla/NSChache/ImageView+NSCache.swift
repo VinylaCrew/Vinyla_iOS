ff46967d1c07b28c72187ce46862b6d55344f775
iJoom <ghfdld@naver.com>
2021-08-30 18:27:04 +0900

    [system] add nscache manager , imageview extension func code

---
+++ Vinyla/Vinyla/NSChache/ImageView+NSCache.swift
@@ -0,0 +1,30 @@
+//
+//  ImageView+NSCache.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/30.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageChache(imageURL: String) {
+        if let cachedImage = NSCacheManager.shaerd.object(forKey: imageURL as NSString) {
+            print("cached image 사용", cachedImage)
+            self.image = cachedImage
+            return
+        }
+
+        DispatchQueue.global(qos: .background).async {
+            guard let insideImageURL = URL(string: imageURL), let imageData = try? Data(contentsOf: insideImageURL), let willBeCachedImage = UIImage(data: imageData) else {
+                return
+            }
+            //백그라운드 스레드로 image data를 얻어오고 메인 스레드에서 캐싱작업과 이미지뷰에 이미지를 넣어주는 작업 진행
+            //백그라운드 스레드 지정을 위해 qos background로 지정 - 기기 효율 에너지와 연관
+            DispatchQueue.main.async {
+                NSCacheManager.shaerd.setObject(willBeCachedImage, forKey: imageURL as NSString)
+                self.image = willBeCachedImage
+            }
+        }
+    }
+}
---
+++ Vinyla/Vinyla/NSChache/NSCacheManager.swift
@@ -0,0 +1,16 @@
+//
+//  NSCacheManager.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/08/30.
+//
+
+import UIKit
+
+class NSCacheManager {
+    static let shaerd = NSCache<NSString, UIImage>()
+
+    private init() {
+
+    }
+}
