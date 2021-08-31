5aac9bcddf141ca8b6cf1135e6f3a9c1d1e5632c
iJoom <ghfdld@naver.com>
2021-08-31 15:42:51 +0900

    add error print in cache manager

--- Vinyla/Vinyla/Modules/LevelDesign/LevelDesignViewController.swift
+++ Vinyla/Vinyla/Modules/LevelDesign/LevelDesignViewController.swift
@@ -25,43 +25,57 @@ final class LevelDesignViewController: UIViewController {
         viewController.coordiNator = coordiNator
         return viewController
     }
-
+    deinit {
+        print("level vc deinit")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         levelDesignTableView.delegate = self
         levelDesignTableView.dataSource = self
         levelDesignTableView.isScrollEnabled = false
         for pointView in levelCirclePointView {
             pointView.layer.cornerRadius = pointView.frame.size.height/2
         }
-        
+        setLabelTextColor()
     }
-
     func setLabelTextColor() {
         guard let levelMentLabelText = levelMentLabel.text else { return  }
         let attributedString = NSMutableAttributedString(string: levelMentLabelText)
 
-//        attributedString.addAttribute(.foregroundColor, value: UIColor.blue, range: (levelMentLabelText as NSString).range(of: "Bonus"))
+        attributedString.addAttribute(.foregroundColor, value: UIColor.white, range: (levelMentLabelText as NSString).range(of: "바이닐을"))
+        attributedString.addAttribute(.foregroundColor, value: UIColor.white, range: (levelMentLabelText as NSString).range(of: "즐기는"))
+        attributedString.addAttribute(.foregroundColor, value: UIColor.white, range: (levelMentLabelText as NSString).range(of: "방법!"))
         attributedString.addAttribute(.foregroundColor, value: UIColor.vinylaMainOrangeColor(), range: (levelMentLabelText as NSString).range(of: "남다르게"))
 
         levelMentLabel.attributedText = attributedString
+        levelMentLabel.font = UIFont(name: "NotoSansKR-Bold", size: 19)
+    }
+    @IBAction func touchUpPOPButton(_ sender: UIButton) {
+        self.coordiNator?.popViewController()
     }
 
 }
 
 extension LevelDesignViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        guard let viewModel = self.viewModel else { return 0 }
+        return viewModel.levelDesignModel.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "LevelDesignCell") as? LevelDesignTableViewCell else {
             return UITableViewCell()
         }
-        if indexPath.row == 0 {
+        if indexPath.row == 2 {
             cell.isGradient = true
         }
+        if let levelImageName = viewModel?.levelDesignModel[indexPath.row].levelImageName {
+            cell.levelImageView.image = UIImage(named: levelImageName)
+        }
+        cell.levelLabel.text = viewModel?.levelDesignModel[indexPath.row].level
+        cell.levelInformationLabel.text = viewModel?.levelDesignModel[indexPath.row].levelName
+        cell.levelMentLabel.text = viewModel?.levelDesignModel[indexPath.row].levelMent
+
         
         return cell
     }
