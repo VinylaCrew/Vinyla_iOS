de1da0f8c53ee1040da629d834340137c7b44f9f
iJoom <ghfdld@naver.com>
2021-09-03 01:19:49 +0900

    create RequestUserVinylViewModel

---
+++ Vinyla/Vinyla/Modules/RequestUserViynl/RequestUserVinylViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RequestUserVinylViewModel.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/09/03.
+//
+
+import Foundation
+
+final class RequestUserVinylViewModel {
+    init() {
+        
+    }
+}
