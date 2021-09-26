8702ed40b2dc9414c9b9efe5454e2b56d3ead471
iJoom <ghfdld@naver.com>
2021-09-26 15:45:25 +0900

    set VinylBoxViewModel Test Code

--- Vinyla/VinylaTests/VinylaTests.swift
+++ Vinyla/VinylaTests/VinylaTests.swift
@@ -7,8 +7,13 @@
 
 import XCTest
 @testable import Vinyla
+import CoreData
 
 class VinylaTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,14 +24,35 @@ class VinylaTests: XCTestCase {
     }
 
     func testExample() throws {
-//        let view = SignUpViewController()
-//        view.setViewModel(MockSignUpViewModel())
-//
-//        XCTAssert(view.)
+        //        let view = SignUpViewController()
+        //        view.setViewModel(MockSignUpViewModel())
+        //
+        //        XCTAssert(view.)
         
         
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testVinylBoxViewModel() {
+        //given
+        var viewModel = VinylBoxViewModel()
+
+//        코어 데이터 삭제 테스트 코드
+//        CoreDataManager.shared.clearAllObjectEntity("VinylBox")
+        //when
+//        for i in 1...10 {
+//            CoreDataManager.shared.saveVinylBox(songTitle: "\(i)", singer: "test", vinylImage: (UIImage(named: "testdog")?.jpegData(compressionQuality: 0))!)
+//        }
+        viewModel.updateVinylBoxesAndReversBoxes()
+        //then
+        print("개수", viewModel.getTotalVinylBoxCount())
+//        XCTAssertEqual(502, viewModel.getTotalVinylBoxCount())
+        print("test paign items",viewModel.getPagingVinylBoxItems(indexPath: IndexPath(item: 0, section: 0)))
+        var testVinylBoxItems = viewModel.getPagingVinylBoxItems(indexPath: [0,1])
+        print("test pg",testVinylBoxItems)
+        print(testVinylBoxItems[0].songTitle)
+        //특정페이지 songTitle로 맞는 데이터들어간지 확인
     }
 
     func testPerformanceExample() throws {
