56b8b29938ab0f136a5464a8bfbf8a942bbcb3fc
iJoom <ghfdld@naver.com>
2021-10-21 01:29:30 +0900

    add base test moya apiservice code

--- Vinyla/VinylaTests/VinylaSearchTests.swift
+++ Vinyla/VinylaTests/VinylaSearchTests.swift
@@ -19,9 +19,27 @@ class VinylaSearchTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSearchAPI() {
+        //given
+        let testAPIService = VinylAPIService()
+
+        let expectation = XCTestExpectation()
+
+        let expectedData = APITarget.vinylSearch(urlParameters: "test").sampleData
+
+        //when
+        testAPIService.testSearchVinyl(vinylName: "asdf")
+            .subscribe(onNext: { data in
+//                XCTAssertEqual(expectedData, data)
+                expectation.fulfill()
+            })
+            .dispose()
+
+        wait(for: [expectation], timeout: 2.0)
+
+
+        //then
+
     }
 
     func testPerformanceExample() throws {
