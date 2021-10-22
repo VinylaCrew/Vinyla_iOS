afd1ee16fdb7fd3f1ff9b1df0b959c5a7c3cf44f
iJoom <ghfdld@naver.com>
2021-10-22 21:50:18 +0900

    [Test] 바이닐 상세정보 API Test API Code 선제구현

--- Vinyla/Vinyla/NetWork/MockAPIService/MockAPIService.swift
+++ Vinyla/Vinyla/NetWork/MockAPIService/MockAPIService.swift
@@ -11,6 +11,7 @@ import Foundation
 
 
 final class MockAPIService: VinylAPIServiceProtocol {
+
     
     static let customEndpointClosure = { (target: APITarget) -> Endpoint in
         return Endpoint(url: URL(target: target).absoluteString,
@@ -26,15 +27,19 @@ final class MockAPIService: VinylAPIServiceProtocol {
         self.provider = provider
     }
 
-    func searchVinyl(vinylName: String) -> Observable<[SearchModel]> {
+    func searchVinyl(vinylName: String) -> Observable<[SearchModel.Data?]> {
+
         return Observable.create() { [weak self] emitter in
             self?.provider.request(.vinylSearch(urlParameters: vinylName)) { result in
                 switch result {
                 case .success(let response):
-                    let decodedData = try? JSONDecoder().decode([SearchModel].self, from: response.data)
-                    print("testSearchVinyl in Method",decodedData)
-                    print("testSearchVinyl response data",response.data)
-                    emitter.onNext(decodedData ?? [])
+                    do {
+                        let decodedData = try JSONDecoder().decode(SearchModel.self, from: response.data)
+                        emitter.onNext(decodedData.data)
+                    }catch {
+                        print("decode error message:",error)
+                    }
+
                 case .failure(let error):
                     emitter.onError(error)
                 }
@@ -45,14 +50,15 @@ final class MockAPIService: VinylAPIServiceProtocol {
     }
 
     func getMovies(order: String) -> Observable<[MovieModel.Data?]> {
+        
         return Observable.create() { [weak self] emitter in
             self?.provider.request(.getMovies(urlParameters: order)) { result in
                 switch result {
                 case .success(let response):
                     do {
                         let decodedData = try JSONDecoder().decode(MovieModel.self, from: response.data)
-//                        print("APIService", decodedData, response.data) // 통신성공 데이터 타입만 맞춰주면됨 [MovieModel] 아님
-//                        print("APIService2", decodedData.movies)
+//                        print("APIService response.data", decodedData, response.data) // 통신성공 데이터 타입만 맞춰주면됨 [MovieModel] 아님
+                        print("APIService2", decodedData.movies)
                         emitter.onNext(decodedData.movies)
                         emitter.onCompleted()
                     } catch {
@@ -67,6 +73,30 @@ final class MockAPIService: VinylAPIServiceProtocol {
             return Disposables.create()
         }
     }
+
+    func getVinylDetail(vinylID: Int?) -> Observable<VinylInformation.Data?> {
+
+        return Observable.create() { [weak self] emitter in
+            self?.provider.request(.getVinylDetail(urlParameters: vinylID)) { result in
+                switch result {
+                case .success(let response):
+                    do {
+                        let decodedData = try JSONDecoder().decode(VinylInformation.self, from: response.data)
+                        emitter.onNext(decodedData.data)
+                        emitter.onCompleted()
+                    } catch {
+                        print("decode error")
+                        print(error.localizedDescription)
+                    }
+                case .failure(let error):
+                    emitter.onError(error)
+                }
+
+            }
+            return Disposables.create()
+        }
+    }
+    
 
 }
 
