01b0c71d62003297eb0278bdba66b9d689fcf88b
iJoom <ghfdld@naver.com>
2021-10-24 22:24:45 +0900

    create AddReviewModel

---
+++ Vinyla/Vinyla/Models/AddReviewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddReviewModel.swift
+//  Vinyla
+//
+//  Created by IJ . on 2021/10/24.
+//
+
+import Foundation
+
+struct AddReviewModel {
+    var songTitle: String?
+    var vinylImageURL: String?
+}
