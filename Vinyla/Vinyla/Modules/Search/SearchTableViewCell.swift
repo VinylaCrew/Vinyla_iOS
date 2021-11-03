0cb27966d3f48a970956db35445da51e41f14afd
iJoom <ghfdld@naver.com>
2021-11-03 16:32:24 +0900

    set ui code

--- Vinyla/Vinyla/Modules/Search/SearchTableViewCell.swift
+++ Vinyla/Vinyla/Modules/Search/SearchTableViewCell.swift
@@ -13,11 +13,44 @@ class SearchTableViewCell: UITableViewCell {
     @IBOutlet weak var songTitleLabel: UILabel!
     @IBOutlet weak var singerNameLabel: UILabel!
     @IBOutlet weak var customSeperatorView: UIView!
-    
+
+    lazy var whiteCircleVinylView: UIView = { () -> UIView in
+        let view = UIView()
+
+        let width: CGFloat = 23
+        let height: CGFloat = 23
+        let positionX: CGFloat = 0
+        let positionY: CGFloat = 0
+        view.frame = CGRect(x: positionX, y: positionY, width: width, height: height)
+        view.backgroundColor = .white
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = view.frame.height/2
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.alpha = 0.5
+
+        let blackCircleView = UIView()
+        blackCircleView.frame = CGRect(x: 0, y: 0, width: 3, height: 3)
+        blackCircleView.backgroundColor = .black
+        blackCircleView.layer.masksToBounds = true
+        blackCircleView.layer.cornerRadius = blackCircleView.frame.height/2
+        view.addSubview(blackCircleView)
+
+        let blackCircleViewHorizontal = blackCircleView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let blackCircleViewVertical = blackCircleView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        let blackCircleViewWidth = blackCircleView.widthAnchor.constraint(equalToConstant: 3)
+        let blackCircleViewHeight = blackCircleView.heightAnchor.constraint(equalToConstant: 3)
+        blackCircleView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addConstraints([blackCircleViewHorizontal, blackCircleViewVertical, blackCircleViewWidth, blackCircleViewHeight])
+
+        return view
+    }()
+
     override func awakeFromNib() {
         super.awakeFromNib()
         searchVinylImageView.layer.cornerRadius = searchVinylImageView.frame.height/2
-        
+        searchVinylImageView.addSubview(whiteCircleVinylView)
+        setAutoLayoutWhiteCircleView()
         customSeperatorView.backgroundColor = UIColor(red: 40/255, green: 40/255, blue: 41/255, alpha: 1)
 //        UIColor(red: 40/255, green: 40/255, blue: 41/255, alpha: 1)
     }
@@ -26,6 +59,20 @@ class SearchTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+//        self.searchVinylImageView.setImageURLAndChaching("Cancel")
+//        self.searchVinylImageView.image = nil
+    }
+
+    func setAutoLayoutWhiteCircleView() {
+        let whiteCircleVinylViewCenterX = whiteCircleVinylView.centerXAnchor.constraint(equalTo: searchVinylImageView.centerXAnchor)
+        let whiteCircleVinylViewCenterY = whiteCircleVinylView.centerYAnchor.constraint(equalTo: searchVinylImageView.centerYAnchor)
+        let whiteCircleVinylViewWidthConstraint = whiteCircleVinylView.widthAnchor.constraint(equalToConstant: 23)
+        let whiteCircleVinylViewHeightConstraint = whiteCircleVinylView.heightAnchor.constraint(equalToConstant: 23)
+        searchVinylImageView.addConstraints([whiteCircleVinylViewCenterX,whiteCircleVinylViewCenterY,whiteCircleVinylViewWidthConstraint,whiteCircleVinylViewHeightConstraint])
     }
     
 }
