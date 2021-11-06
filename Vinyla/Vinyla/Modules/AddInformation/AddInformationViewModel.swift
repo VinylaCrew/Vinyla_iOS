2793f15a04d45aae705bd9447cadca1af5098aa1
iJoom <ghfdld@naver.com>
2021-11-06 15:22:52 +0900

    feat(addinformation): add rx network code

--- Vinyla/Vinyla/Modules/AddInformation/AddInformationViewModel.swift
+++ Vinyla/Vinyla/Modules/AddInformation/AddInformationViewModel.swift
@@ -6,11 +6,29 @@
 //
 
 import Foundation
+import RxSwift
 
 class AddInformationViewModel {
     var model: AddInformationModel
+    var vinylInformationData: BehaviorSubject<VinylInformation.Data?> = BehaviorSubject<VinylInformation.Data?>(value: nil)
+    var vinylInformationService: VinylAPIServiceProtocol
 
-    init() {
+    var disposeBag = DisposeBag()
+    init(vinylInformationService: VinylAPIServiceProtocol = VinylAPIService()) {
         self.model = AddInformationModel()
+        self.vinylInformationService = vinylInformationService
+
+        _ = self.vinylInformationService.getVinylDetail(vinylID: 2865072)
+            .bind(to: vinylInformationData)
+//            .bind(to: vinylInformationData)
+    }
+
+    func fetchVinylInformation() {
+        print("hihi")
+        self.vinylInformationService.getVinylDetail(vinylID: 18048907)
+            .subscribe(onNext: { data in
+                print("fetch",data!)
+            })
+            .disposed(by: disposeBag)
     }
 }
