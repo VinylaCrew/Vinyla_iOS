202d16dcf0add3f57327df70739d2a2f1d37b578
iJoom <ghfdld@naver.com>
2021-11-23 09:58:28 +0900

    edit vinyl information model

--- Vinyla/Vinyla/Models/VinylInformation.swift
+++ Vinyla/Vinyla/Models/VinylInformation.swift
@@ -10,28 +10,17 @@ import Foundation
 struct VinylInformation: Codable {
 
     struct Data: Codable {
-        let id: Int
-        let title, artist: String
-        let image: String
-        let year: Int
-        let genres: [String]
-        let tracklist: [Tracklist]
-        let rate, rateCount: Int
+        let id: Int?
+        let title, artist: String?
+        let image: String?
+        let year: Int?
+        let genres: [String]?
+        let tracklist: [String]?
+        let rate, rateCount: Int?
     }
 
-    let status: Int
-    let success: Bool
-    let message: String
-    let data: Data
-}
-
-
-struct Tracklist: Codable {
-    let position, type, title, duration: String
-
-    enum CodingKeys: String, CodingKey {
-        case position
-        case type = "type_"
-        case title, duration
-    }
+    let status: Int?
+    let success: Bool?
+    let message: String?
+    let data: Data?
 }
