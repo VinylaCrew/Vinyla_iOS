36724f2f6de1b57c962d8e106d7d220c480104f0
iJoom <ghfdld@naver.com>
2021-11-26 15:52:34 +0900

    add Vinyl ID

--- Vinyla/Vinyla/Models/AddInformationModel.swift
+++ Vinyla/Vinyla/Models/AddInformationModel.swift
@@ -10,8 +10,12 @@ import Foundation
 struct AddInformationModel {
     var vinylTitleSong: String?
     var vinylImageURL: String?
+    var vinylID: Int?
+    var vinylTrackList: [String?]
     init() {
         self.vinylTitleSong = ""
         self.vinylImageURL = ""
+        self.vinylID = 0
+        self.vinylTrackList = []
     }
 }
