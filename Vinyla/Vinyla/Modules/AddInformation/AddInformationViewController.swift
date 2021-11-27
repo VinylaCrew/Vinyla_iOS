d4e3c0048838addec096af7126e05e9a8064ea1f
iJoom <ghfdld@naver.com>
2021-11-27 15:37:33 +0900

    add no vinyl detail data case , unabled button and label

--- Vinyla/Vinyla/Modules/AddInformation/AddInformationViewController.swift
+++ Vinyla/Vinyla/Modules/AddInformation/AddInformationViewController.swift
@@ -56,18 +56,22 @@ class AddInformationViewController: UIViewController {
     }()
 
     @IBOutlet weak var songTitleLabel: UILabel!
+    @IBOutlet weak var songArtistLabel: UILabel!
+    @IBOutlet weak var songReleaseDataLabel: UILabel!
+    @IBOutlet weak var songGenresLabel: UILabel!
+    @IBOutlet weak var songCountLabel: UILabel!
+
     @IBOutlet weak var informationScrollView: UIScrollView!
     @IBOutlet weak var albumSongListTableView: UITableView!
     @IBOutlet weak var albumSongListTableViewHeight: NSLayoutConstraint!
     @IBOutlet weak var blurCircleView: BlurCircleView!
 
-    
-    
+
     private weak var coordiNator: AppCoordinator?
     private var viewModel: AddInformationViewModel?
     var disposebag = DisposeBag()
     
-    var dataCount: Int = 1
+    var dataCount: Int = 5
     
     static func instantiate(viewModel: AddInformationViewModel, coordiNator: AppCoordinator) -> UIViewController {
         let storyBoard = UIStoryboard(name: "AddInformation", bundle: nil)
@@ -81,26 +85,33 @@ class AddInformationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       setAlbumListTableViewUI()
+
+        setAlbumListTableViewUI()
         self.view.addSubview(saveInformationButton)
         saveInformationButton.rx.tap.subscribe(onNext:  { [weak self] in
-
-            self?.coordiNator?.moveToAddReview(vinylDataModel: AddReviewModel.init(songTitle: self?.viewModel?.model.vinylTitleSong, vinylImageURL: self?.viewModel?.model.vinylImageURL))
+            guard let vinylInformationModel = self?.viewModel?.vinylInformationDataModel else { return }
+            let viniylDetailData = AddReviewModel.init(vinylImageURL: self?.viewModel?.model.vinylImageURL, songTitle: vinylInformationModel.title, songArtist: vinylInformationModel.artist, rate: vinylInformationModel.rate, rateCount: vinylInformationModel.rateCount)
+            self?.coordiNator?.moveToAddReview(vinylDataModel: viniylDetailData)
         }).disposed(by: disposebag)
         
         informationScrollView.delegate = self
         blurCircleView.delegate = self
-        
-        songTitleLabel.text = viewModel?.model.vinylTitleSong
 
         guard let vinylImageURL = viewModel?.model.vinylImageURL else { return }
         blurCircleView.shownCircleImageView.setImageChache(imageURL: vinylImageURL)
         blurCircleView.backgroundImageView.setImageChache(imageURL: vinylImageURL)
+
+        bindViynlInformationData()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
-        bindViynlInformationData()
+        print("view will",viewModel!.model.vinylID,viewModel?.model.vinylTrackList.count)
+        viewModel?.fetchVinylInformation()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDid",viewModel?.model.vinylTrackList,viewModel?.model.vinylTitleSong)
+        self.albumSongListTableViewHeight.constant = CGFloat((viewModel?.model.vinylTrackList.count)!*21)
     }
     func setAlbumListTableViewUI() {
         albumSongListTableView.delegate = self
@@ -111,7 +122,7 @@ class AddInformationViewController: UIViewController {
         albumSongListTableView.estimatedRowHeight = 24
         albumSongListTableView.separatorStyle = .none
         albumSongListTableView.backgroundColor = .black
-        albumSongListTableViewHeight.constant = CGFloat(dataCount*21) // 21size가 추가셀 생성되지 않음
+//        albumSongListTableViewHeight.constant = CGFloat(dataCount*21) // 21size가 추가셀 생성되지 않음
         albumSongListTableView.isScrollEnabled = false
     }
     func bindViynlInformationData() {
@@ -119,7 +130,17 @@ class AddInformationViewController: UIViewController {
             .observeOn(MainScheduler.instance)
             .subscribe(onNext: { [weak self] data in
                 print("bindViynlInformationData",data)
-                guard let vinylInformation = data else { return }
+                guard let vinylInformation = data else {
+                    self?.songTitleLabel.text = "상세조회 데이터가 없습니다."
+                    self?.saveInformationButton.isEnabled = false
+                    self?.saveInformationButton.backgroundColor = UIColor.buttonDisabledColor()
+                    return
+                }
+                //tracklist tableview
+                self?.viewModel?.model.vinylTrackList = vinylInformation.tracklist!
+                self?.albumSongListTableViewHeight.constant = CGFloat((self?.viewModel?.model.vinylTrackList.count)!*21)
+                self?.albumSongListTableView.reloadData()
+
                 if let imageURL = vinylInformation.image {
                     self?.blurCircleView.shownCircleImageView.setImageURLAndChaching(imageURL)
                     self?.blurCircleView.backgroundImageView.setImageURLAndChaching(imageURL)
@@ -128,6 +149,15 @@ class AddInformationViewController: UIViewController {
                     self?.blurCircleView.backgroundImageView.image = UIImage()
                 }
                 self?.songTitleLabel.text = vinylInformation.title
+                self?.songArtistLabel.text = vinylInformation.artist
+                self?.songGenresLabel.text = vinylInformation.genres?[0]
+                self?.songCountLabel.text = String(vinylInformation.tracklist?.count ?? 0)
+                self?.songReleaseDataLabel.text = String(vinylInformation.year ?? 0)
+
+                //save button enabled setting
+                self?.saveInformationButton.isEnabled = true
+                self?.saveInformationButton.backgroundColor = UIColor.buttonOrangeColor()
+
             })
             .disposed(by: disposebag)
         
@@ -142,13 +172,16 @@ class AddInformationViewController: UIViewController {
 
 extension AddInformationViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataCount
+        print("count",viewModel!.model.vinylTrackList.count)
+        return viewModel!.model.vinylTrackList.count
+        //return dataCount
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "albumSongListCell") as? AlbumSongListTableViewCell else { return UITableViewCell() }
         cell.backgroundColor = .black
-        cell.albumSongTitleLabel.text = "\(indexPath.row)"
+        cell.albumSongTitleLabel.text = viewModel?.model.vinylTrackList[indexPath.row]
+//        cell.albumSongTitleLabel.text = "\(indexPath.row)"
         cell.selectionStyle = .none
         return cell
     }
