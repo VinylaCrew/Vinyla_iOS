57b2e0c15820f06ba7f3264deef88b47538df5f7
iJoom <ghfdld@naver.com>
2021-11-29 21:59:43 +0900

    delete unused variable

--- Vinyla/Vinyla/Modules/Search/SearchViewModel.swift
+++ Vinyla/Vinyla/Modules/Search/SearchViewModel.swift
@@ -10,26 +10,25 @@ import RxSwift
 
 protocol SearchViewModelType {
     //Input
-    var vinylName: BehaviorSubject<String> { get }
+    var vinylName: PublishSubject<String> { get }
     var orderNumber: BehaviorSubject<String> { get }
 
     //Output
     var vinylsData: BehaviorSubject<[SearchModel.Data?]> { get }
     var isSearch: PublishSubject<Bool> { get }
-    var moviesData: BehaviorSubject<MovieModel.Data?> { get }
+    var vinylsCount: BehaviorSubject<String?> { get set }
 
     var searchAPIService: VinylAPIServiceProtocol { get set }
 }
 
-final class SearchViewModel {
+final class SearchViewModel: SearchViewModelType {
     //Input
     var vinylName: PublishSubject<String> = PublishSubject<String>()
     var orderNumber: BehaviorSubject<String> = BehaviorSubject<String>(value: "")
 
     //Output
-    var moviesData: BehaviorSubject<[MovieModel.Data?]> = BehaviorSubject<[MovieModel.Data?]>(value: [])
     public private(set) var vinylsData: BehaviorSubject<[SearchModel.Data?]> = BehaviorSubject<[SearchModel.Data?]>(value: [])
-    var vinylsCount: BehaviorSubject<String?> = BehaviorSubject<String?>(value: "")
+    var vinylsCount: BehaviorSubject<String?> = BehaviorSubject<String?>(value: "0")
     var searchAPIService: VinylAPIServiceProtocol
     var isSearch: PublishSubject<Bool> = PublishSubject<Bool>()
     var disposeBag = DisposeBag()
@@ -41,8 +40,8 @@ final class SearchViewModel {
             .do(onNext: { [weak self] _ in self?.isSearch.onNext(true) })
             .flatMapLatest{ [unowned self] vinyl -> Observable<[SearchModel.Data?]> in
                 print("vinylName vimodel test:",vinyl)
-//                return self.searchAPIService.searchVinyl(vinylName: vinyl)
-                return testAPIService.searchVinyl(vinylName: vinyl)
+                return self.searchAPIService.searchVinyl(vinylName: vinyl)
+//                return testAPIService.searchVinyl(vinylName: vinyl)
             }
             .do(onNext: { [weak self] _ in self?.isSearch.onNext(false) })
             .bind(to: vinylsData)
