69a32ee1aa8e78f5c45898f65fc9c41ac9e8ec39
iJoom <ghfdld@naver.com>
2021-12-06 20:04:20 +0900

    delete: 불필요한 주석 및 코드 제거

--- Vinyla/Vinyla/NetWork/APIService.swift
+++ Vinyla/Vinyla/NetWork/APIService.swift
@@ -23,23 +23,6 @@ final class VinylAPIService: VinylAPIServiceProtocol {
         self.provider = provider
     }
 
-    //test 이전 틀렸던 코드
-//    func searchVinyl(vinylName: String) -> Observable<[SearchModel]> {
-//        return Observable.create() { [weak self] emitter in
-//            self?.provider.request(.vinylSearch(urlParameters: vinylName)) { result in
-//                switch result {
-//                case .success(let response):
-//                    let decodedData = try? JSONDecoder().decode([SearchModel].self, from: response.data)
-//                    emitter.onNext(decodedData ?? [])
-//                case .failure(let error):
-//                    emitter.onError(error)
-//                }
-//                emitter.onCompleted()
-//            }
-//            return Disposables.create()
-//        }
-//    }
-
     func searchVinyl(vinylName: String) -> Observable<[SearchModel.Data?]> {
 
         return Observable.create() { [weak self] emitter in
@@ -88,37 +71,6 @@ final class VinylAPIService: VinylAPIServiceProtocol {
             return Disposables.create()
         }
     }
-
-//    func getMovies(order: String) -> Observable<[MovieModel.Data?]> {
-//
-////        if order == "" { return Observable.create() { ob in
-////            ob.onNext([])
-////            ob.onCompleted()
-////            return Disposables.create()
-////        }
-////        }
-//        return Observable.create() { [weak self] emitter in
-//            self?.provider.request(.getMovies(urlParameters: order)) { result in
-//                switch result {
-//                case .success(let response):
-//                    do {
-//                        let decodedData = try JSONDecoder().decode(MovieModel.self, from: response.data)
-////                        print("APIService response.data", decodedData, response.data) // 통신성공 데이터 타입만 맞춰주면됨 [MovieModel] 아님
-//                        print("APIService2", decodedData.movies)
-//                        emitter.onNext(decodedData.movies)
-//                        emitter.onCompleted()
-//                    } catch {
-//                        print("decode error")
-//                        print(error.localizedDescription)
-//                    }
-//
-//                case .failure(let error):
-//                    emitter.onError(error)
-//                }
-//            }
-//            return Disposables.create()
-//        }
-//    }
 
     func getVinylDetail(vinylID: Int?) -> Observable<VinylInformation.Data?> {
 
