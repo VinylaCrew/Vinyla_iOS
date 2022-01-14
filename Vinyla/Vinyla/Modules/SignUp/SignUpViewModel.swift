ac50833b08a6bac2d2c2acac048d8db3f00b46d9
iJoom <ghfdld@naver.com>
2022-01-15 00:04:06 +0900

    refactoring: logic code

--- Vinyla/Vinyla/Modules/SignUp/SignUpViewModel.swift
+++ Vinyla/Vinyla/Modules/SignUp/SignUpViewModel.swift
@@ -9,32 +9,51 @@ import Foundation
 import RxSwift
 
 protocol SignUpViewModelProtocol {
-    var nickNameText: PublishSubject<String> { get }
+    var nicknameText: PublishSubject<String> { get }
     var isValidNickNameNumberSubject: BehaviorSubject<Int> { get }
     var validNickNameNumberSubject: PublishSubject<Int> { get }
     var isValidNickNameNumber: Int? { get }
     func isValidNickName(_ nickNameText: String) -> Int
-    func checkString(text:String) -> Bool
 }
 
 final class SignUpViewModel: SignUpViewModelProtocol {
     //input
-    private(set) var nickNameText: PublishSubject<String> = PublishSubject<String>()
+    private(set) var nicknameText: PublishSubject<String> = PublishSubject<String>()
     private(set) var isValidNickNameNumberSubject = BehaviorSubject<Int>(value: -5)
     //output
     private(set) var validNickNameNumberSubject = PublishSubject<Int>()
     public private(set) var isValidNickNameNumber: Int? = -1
-    var signUpModel = SignUpModel()
+    var signUpAPIService: VinylAPIService
     var disposeBag = DisposeBag()
 
-    init() {
+    init(signUpAPIService: VinylAPIService = VinylAPIService()) {
+        self.signUpAPIService = signUpAPIService
+        
         // input textfield text를 , 유효한 닉네임인지 검사해서 Int값으로 Output 출력 바인딩
-        _ = nickNameText.map{ [unowned self] text in
+        _ = nicknameText.map{ [unowned self] text in
             return self.isValidNickName(text)
         }
         .bind(to: validNickNameNumberSubject)
         .disposed(by: disposeBag)
 
+
+        // Server Nickname 중복검사
+        _ = self.nicknameText
+            .filter{ $0.count >= 2}
+            .filter{ self.isValidNickName($0) == 1}
+            .flatMapLatest{ [unowned self] userNickname -> Observable<NickNameCheckResponse.Data?> in
+                let nickNameCheckRequest = NickNameCheckRequest(nickname: userNickname)
+                print("signUpAPIService.checkNickName")
+                return self.signUpAPIService.checkNickName(requestModel: nickNameCheckRequest)
+            }
+            .subscribe(onNext:{ data in
+                if let data = data {
+                    print(data)
+                }else {
+                    self.validNickNameNumberSubject.onNext(4)
+                }
+            })
+            .disposed(by: disposeBag)
     }
     deinit {
         print("SignUpViewModel deinit")
@@ -66,27 +85,11 @@ final class SignUpViewModel: SignUpViewModelProtocol {
 
         if isValidNickNameValue == 1 {
             //통신해서 가능한 닉네임인지 아닌지 체크
+
         }
 
+        self.isValidNickNameNumber = isValidNickNameValue
+
         return isValidNickNameValue
-    }
-    
-    func checkString(text:String) -> Bool {
-        // String -> Array
-        let arr = Array(text)
-        // 정규식 pattern. 한글, 영어, 숫자, 밑줄(_)만 있어야함
-        let pattern = "^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9_]$"
-        if let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive) {
-            var index = 0
-            while index < arr.count { // string 내 각 문자 하나하나 마다 정규식 체크 후 충족하지 못한것은 제거.
-                let results = regex.matches(in: String(arr[index]), options: [], range: NSRange(location: 0, length: 1))
-                if results.count == 0 {
-                    return false
-                } else {
-                    index += 1
-                }
-            }
-        }
-        return true
     }
 }
