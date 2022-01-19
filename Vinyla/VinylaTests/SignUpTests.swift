c541bb3a473f5dc2de25134a026bf81307cdcb8a
iJoom <ghfdld@naver.com>
2022-01-19 12:43:26 +0900

    add: simple nick name check func tests

--- Vinyla/VinylaTests/SignUpTests.swift
+++ Vinyla/VinylaTests/SignUpTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Vinyla
 
 class SignUpTests: XCTestCase {
 
@@ -13,13 +14,17 @@ class SignUpTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testSignUpCase1() throws {
+        let testViewModel = SignUpViewModel(signUpAPIService: VinylAPIService())
+
+        let testValue1 = testViewModel.isValidNickName("jj")
+        let testValue2 = testViewModel.isValidNickName("Testㄱㄴ")
+
+        XCTAssertEqual(testValue1, 1)
+        XCTAssertEqual(testValue2, 3)
+
     }
 
     func testPerformanceExample() throws {
