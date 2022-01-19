c205c969c3a4f0c33f73583a50b68fbb2043b507
iJoom <ghfdld@naver.com>
2022-01-19 14:41:53 +0900

    add: test code

--- Vinyla/VinylaTests/SignUpTests.swift
+++ Vinyla/VinylaTests/SignUpTests.swift
@@ -7,23 +7,43 @@
 
 import XCTest
 @testable import Vinyla
+import RxSwift
 
 class SignUpTests: XCTestCase {
 
+    var disposeBag = DisposeBag()
+    var testViewModel: SignUpViewModel!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testViewModel = SignUpViewModel(signUpAPIService: VinylAPIService())
     }
 
-
-
     func testSignUpCase1() throws {
-        let testViewModel = SignUpViewModel(signUpAPIService: VinylAPIService())
 
         let testValue1 = testViewModel.isValidNickName("jj")
         let testValue2 = testViewModel.isValidNickName("Testㄱㄴ")
 
+        let expectation = XCTestExpectation(description: "API Request")
+        let nickNameRequest = NickNameCheckRequest(nickname: "test")
+
+        _ = testViewModel.signUpAPIService.checkNickName(requestModel: nickNameRequest)
+            .subscribe(onNext: { data in
+                if let data = data {
+                    //닉네임 중복되지 않음
+                    XCTAssertNotNil(data)
+                }else {
+                    //닉네임 중복
+                    XCTAssertNil(data)
+                }
+
+                expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+
         XCTAssertEqual(testValue1, 1)
         XCTAssertEqual(testValue2, 3)
+
+        wait(for: [expectation], timeout: 10)
 
     }
 
