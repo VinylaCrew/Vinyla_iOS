59b86e2316125d8b2446c6d49aea34e616a2c1c8
iJoom <ghfdld@naver.com>
2022-01-26 22:22:12 +0900

    add: NetworkManger server api test codes

--- Vinyla/VinylaTests/NetworkManagerTests.swift
+++ Vinyla/VinylaTests/NetworkManagerTests.swift
@@ -6,20 +6,42 @@
 //
 
 import XCTest
+@testable import Vinyla
+import RxSwift
 
 class NetworkManagerTests: XCTestCase {
+
+    var disposeBag = DisposeBag()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testNetworkManager() {
+        var expectation = XCTestExpectation(description: "NicknameServerAPI")
+        var searchExpectation = XCTestExpectation(description: "SearchServerAPI")
+        let nicknameAPI = APITarget.checkNickName(body: NickNameCheckRequest(nickname: "tes"))
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        NetworkManager.request(apiType: nicknameAPI)
+            .subscribe(onSuccess: { (successModel: NickNameCheckResponse) in
+                print(successModel.message)
+                XCTAssertEqual(successModel.message,"사용 가능한 닉네임입니다.")
+                expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+
+        let searchAPI = APITarget.vinylSearch(urlParameters: "hh")
+
+        NetworkManager.request(apiType: searchAPI)
+            .subscribe(onSuccess: { (responseData: SearchModel) in
+                print(responseData.message)
+                XCTAssertEqual(responseData.message, "바이닐 검색 성공")
+                XCTAssertNotNil(responseData.data)
+                searchExpectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+
+        wait(for: [expectation,searchExpectation], timeout: 20)
     }
 
     func testPerformanceExample() throws {
