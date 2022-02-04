3cd4c3f0447d84bb44fcb4e78735dd49f70970be
iJoom <ghfdld@naver.com>
2022-02-04 17:54:19 +0900

    create: user sign in logic test 코드 추가 - 성공 케이스, 필요한 body 값이 없어 실패하는 케이스

---
+++ Vinyla/VinylaTests/SignInTests.swift
@@ -0,0 +1,58 @@
+//
+//  SignInTests.swift
+//  VinylaTests
+//
+//  Created by IJ . on 2022/02/04.
+//
+
+import XCTest
+@testable import Vinyla
+import RxSwift
+
+class SignInTests: XCTestCase {
+
+    var disposeBag: DisposeBag!
+    var expectation: XCTestExpectation!
+
+    override func setUpWithError() throws {
+        self.disposeBag = DisposeBag()
+        expectation = XCTestExpectation(description: "SignInAPITest")
+    }
+    func testSignInSuccessCase() {
+        let signinAPI = APITarget.signinUser(userToken: SignInRequest(fuid: "test151515", fcmToken: "123456"))
+
+        CommonNetworkManager.request(apiType: signinAPI)
+            .subscribe(onSuccess:{ [weak self] (response: SignInResponse) in
+                XCTAssertNotNil(response)
+                self?.expectation.fulfill()
+            },onError: { [weak self] error in
+                print("success error",error)
+                self?.expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testSignInFailureCase() {
+        let signinAPI = APITarget.signinUser(userToken: SignInRequest(fuid: "", fcmToken: ""))
+
+        CommonNetworkManager.request(apiType: signinAPI)
+            .subscribe(onSuccess:{ [weak self] (response: SignInResponse) in
+                self?.expectation.fulfill()
+            },onError: { error in
+                XCTAssertEqual(error as? NetworkError, NetworkError.bodyDataError)
+                self.expectation.fulfill()
+            })
+            .disposed(by: disposeBag)
+
+        wait(for: [expectation], timeout: 5)
+    }
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
