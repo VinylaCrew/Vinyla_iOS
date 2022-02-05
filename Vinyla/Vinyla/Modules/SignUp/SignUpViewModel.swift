56b6e7c194e0b56a362c0bdf3747e570d5bc060a
iJoom <ghfdld@naver.com>
2022-02-06 01:04:10 +0900

    add: request create user 로직 추가

--- Vinyla/Vinyla/Modules/SignUp/SignUpViewModel.swift
+++ Vinyla/Vinyla/Modules/SignUp/SignUpViewModel.swift
@@ -7,23 +7,31 @@
 
 import Foundation
 import RxSwift
+import Firebase
 
 protocol SignUpViewModelProtocol {
     var nicknameText: PublishSubject<String> { get }
+    var instagramIDText: BehaviorSubject<String> { get }
     var validNickNameNumberSubject: PublishSubject<Int> { get }
     var isValidNickNameNumber: Int? { get }
     var checkNickNameNumberSubject: PublishSubject<Int> { get }
+    var isCompletedCreateUserRequest: PublishSubject<Bool> { get }
     func isValidNickName(_ nickNameText: String) -> Int
+//    func testStreamMethod() -> Void
+    func requestCreateUser() -> Void
 }
 
 final class SignUpViewModel: SignUpViewModelProtocol {
     //input
     private(set) var nicknameText: PublishSubject<String> = PublishSubject<String>()
+    private(set) var instagramIDText: BehaviorSubject<String> = BehaviorSubject<String>(value: "")
     //output
     private(set) var validNickNameNumberSubject = PublishSubject<Int>()
     private(set) var isUniqueNickNameSubject = PublishSubject<Int>()
     private(set) var checkNickNameNumberSubject = PublishSubject<Int>()
+    private(set) var isCompletedCreateUserRequest: PublishSubject<Bool> = PublishSubject<Bool>()
     public private(set) var isValidNickNameNumber: Int? = -1
+    private(set) var userNickName: String?
 
     var signUpAPIService: VinylAPIService
     var disposeBag = DisposeBag()
@@ -38,23 +46,23 @@ final class SignUpViewModel: SignUpViewModelProtocol {
         .bind(to: validNickNameNumberSubject)
         .disposed(by: disposeBag)
 
-
         // Server Nickname 중복검사
         _ = self.nicknameText
-//            .filter{ $0.count >= 2}
-//            .filter{ self.isValidNickName($0) == 1}
+            //            .filter{ $0.count >= 2}
+            //            .filter{ self.isValidNickName($0) == 1}
             .flatMapLatest{ [unowned self] userNickname -> Observable<NickNameCheckResponse.Data?> in
                 let nickNameCheckRequest = NickNameCheckRequest(nickname: userNickname)
                 print("signUpAPIService.checkNickName")
-                return self.signUpAPIService.checkNickName(requestModel: nickNameCheckRequest)
+                return self.signUpAPIService.requestCheckNickName(requestModel: nickNameCheckRequest)
             }
             .subscribe(onNext:{ [weak self] data in
                 if let data = data {
                     print(data)
+                    self?.userNickName = data.nickname
                     self?.isUniqueNickNameSubject.onNext(1)
                 }else {
                     //닉네임 중복
-//                    self?.validNickNameNumberSubject.onNext(4)
+                    //                    self?.validNickNameNumberSubject.onNext(4)
                     self?.isUniqueNickNameSubject.onNext(4)
                     self?.isValidNickNameNumber = 4
                 }
@@ -79,6 +87,27 @@ final class SignUpViewModel: SignUpViewModelProtocol {
     deinit {
         print("SignUpViewModel deinit")
     }
+
+    func requestCreateUser() {
+        guard let nickName = self.userNickName, let firebaseUid = Auth.auth().currentUser?.uid, let instagramID = try? self.instagramIDText.value() else {
+            return
+        }
+
+        let userData = SignUpRequest(fuid: "test111", sns: "google", nickname: nickName, instaId: instagramID, fcmToken: "testfcmtoken", subscribeAgreed: 1)
+
+        let createUserAPI = APITarget.createUser(userData: userData)
+        _ = CommonNetworkManager.request(apiType: createUserAPI)
+            .subscribe(onSuccess: { [weak self] (response: SignUpResponse) in
+                print(response)
+                //MARK: TODO 바닐라 토큰 유저 디폴트 저장
+                UserDefaults.standard.setValue(response.data?.token, forKey: UserDefaultsKey.vinylaToken)
+                self?.isCompletedCreateUserRequest.onNext(true)
+            }, onError: { error in
+                print(error)
+            })
+            .disposed(by: disposeBag)
+    }
+
     
     func isValidNickName(_ nickNameText: String) -> Int {
 
@@ -109,16 +138,16 @@ final class SignUpViewModel: SignUpViewModelProtocol {
 
         if isValidNickNameValue == 1 {
             //통신해서 가능한 닉네임인지 아닌지 체크
-//            let nickNameCheckRequest = NickNameCheckRequest(nickname: nickNameText)
-//            self.signUpAPIService.checkNickName(requestModel: nickNameCheckRequest)
-//                .subscribe(onNext:{ [weak self] data in
-//                    if data == nil {
-//                        self?.isValidNickNameNumber = 4
-//                        self?.validNickNameNumberSubject.onNext(4)
-//                    }
-//
-//                })
-//                .disposed(by: disposeBag)
+            //            let nickNameCheckRequest = NickNameCheckRequest(nickname: nickNameText)
+            //            self.signUpAPIService.checkNickName(requestModel: nickNameCheckRequest)
+            //                .subscribe(onNext:{ [weak self] data in
+            //                    if data == nil {
+            //                        self?.isValidNickNameNumber = 4
+            //                        self?.validNickNameNumberSubject.onNext(4)
+            //                    }
+            //
+            //                })
+            //                .disposed(by: disposeBag)
 
         }
 
