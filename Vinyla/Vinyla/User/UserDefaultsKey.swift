20af2deafaa028225d7e48229bbb8a7d950d1351
iJoom <ghfdld@naver.com>
2022-02-06 01:00:37 +0900

    create: user defaults key관리하는 struct 추가

---
+++ Vinyla/Vinyla/User/UserDefaultsKey.swift
@@ -0,0 +1,13 @@
+//
+//  UserDefaultsKey.swift
+//  Vinyla
+//
+//  Created by IJ . on 2022/02/06.
+//
+
+import Foundation
+
+struct UserDefaultsKey {
+    static let vinylaToken = "VinylaToken"
+    static let initIsFirstLogIn = "IsFirstLogin"
+}
