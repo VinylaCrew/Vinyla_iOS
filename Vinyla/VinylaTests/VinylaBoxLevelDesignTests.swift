3f22b56791643036cb6243e7731a7152b5c73231
iJoom <ghfdld@naver.com>
2022-02-26 17:36:18 +0900

    delete: 불필요 코드 제거

--- Vinyla/VinylaTests/VinylaBoxLevelDesignTests.swift
+++ Vinyla/VinylaTests/VinylaBoxLevelDesignTests.swift
@@ -69,7 +69,6 @@ class VinylaBoxLevelDesignTests: XCTestCase {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
-            testVinylBoxViewModel()
         }
     }
 
