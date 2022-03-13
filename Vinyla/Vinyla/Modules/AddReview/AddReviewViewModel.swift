fc2690f75b2ad0e72c47cf0c1b1b3302d85ee0ce
iJoom <ghfdld@naver.com>
2022-03-13 15:43:22 +0900

    add: 서버에 저장 요청 완료 후 코어데이터 캐싱 저장 로직 추가

--- Vinyla/Vinyla/Modules/AddReview/AddReviewViewModel.swift
+++ Vinyla/Vinyla/Modules/AddReview/AddReviewViewModel.swift
@@ -6,10 +6,68 @@
 //
 
 import Foundation
+import RxSwift
 
-final class AddReviewViewModel {
-    var model: AddReviewModel
+protocol AddReviewViewModelProtocol {
+    //input
+    var userRate: PublishSubject<Int> { get }
+    var userCommnet: PublishSubject<String> { get }
+}
+
+final class AddReviewViewModel: AddReviewViewModelProtocol {
+    //input
+    var userRate: PublishSubject<Int> = PublishSubject<Int>()
+    var userCommnet: PublishSubject<String> = PublishSubject<String>()
+    //output
+
+    //model
+    var model: RequestSaveVinylModel
+    var thumbnailImage: String?
+    var disposeBag = DisposeBag()
     init() {
-        self.model = AddReviewModel()
+        self.model = RequestSaveVinylModel.init(id: 0, title: "", artist: "", image: "", year: 0, genres: [], tracklist: [], rate: nil, comment: nil)
+
+        _ = userRate.subscribe(onNext: { [weak self] rate in
+            self?.model.rate = rate
+        })
+        .disposed(by: disposeBag)
+
+        _ = userCommnet.subscribe(onNext:{ [weak self] commnet in
+            self?.model.comment = commnet
+        })
+        .disposed(by: disposeBag)
+    }
+
+    func requestSaveVinylData(dispatchGroup: DispatchGroup) {
+        let saveViynlAPI = APITarget.saveVinyl(vinylData: self.model)
+
+        CommonNetworkManager.request(apiType: saveViynlAPI)
+            .subscribe(onSuccess: { [weak self] (response: ResponseSaveVinylModel) in
+                print(response)
+                let checkSaveDispatchGroup = DispatchGroup()
+
+                if let vinylIndex = response.data?.vinylIdx {
+                    DispatchQueue.global().async() {
+                        checkSaveDispatchGroup.enter()
+                        guard let insideImageURL = URL(string: (self?.thumbnailImage)!) else { return }
+                        let dataTask = URLSession.shared.dataTask(with: insideImageURL) { (data, result, error) in
+                            guard error == nil else {
+                                return
+                            }
+
+                            if let data = data, let vinylImage = UIImage(data: data) {
+                                CoreDataManager.shared.saveVinylBoxWithDispatchGroup(vinylIndex: Int32(vinylIndex), vinylID: Int64((self?.model.id)!), songTitle: self?.model.title ?? "", singer: self?.model.artist ?? "", vinylImage: vinylImage.jpegData(compressionQuality: 1)!, dispatchGroup: checkSaveDispatchGroup)
+                            }
+                        }
+
+                        dataTask.resume()
+                    }
+                }
+
+                checkSaveDispatchGroup.notify(queue: .global()) {
+                    dispatchGroup.leave()
+                }
+            })
+            .disposed(by: disposeBag)
     }
 }
