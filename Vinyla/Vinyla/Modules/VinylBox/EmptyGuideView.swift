10951f71f17145470b0c9e8f2d3c07dd703ae453
iJoom <ghfdld@naver.com>
2022-04-04 23:10:25 +0900

    create: empty guide view

---
+++ Vinyla/Vinyla/Modules/VinylBox/EmptyGuideView.swift
@@ -0,0 +1,22 @@
+//
+//  EmptyGuidView.swift
+//  Vinyla
+//
+//  Created by IJ . on 2022/04/04.
+//
+
+import UIKit
+
+class EmptyGuidView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+    }
+
+}
