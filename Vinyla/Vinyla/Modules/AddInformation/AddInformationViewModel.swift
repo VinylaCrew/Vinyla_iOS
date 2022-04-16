57c7d1a1fe07797796299d51fb25537e85f2dbaf
iJoom <ghfdld@naver.com>
2022-04-16 22:15:28 +0900

    feat: 바이닐 통신 삭제 완료 후 코어 데이터 삭제 로직 구현 완료

--- Vinyla/Vinyla/Modules/AddInformation/AddInformationViewModel.swift
+++ Vinyla/Vinyla/Modules/AddInformation/AddInformationViewModel.swift
@@ -10,6 +10,8 @@ import RxSwift
 
 class AddInformationViewModel {
     var model: AddInformationModel
+    var isDeleteMode: Bool?
+    //vinyl 상세 데이터 정보 (Server 바이닐 저장 Request에 필요 정보)
     var vinylInformationDataModel: VinylInformation.Data?
     var vinylInformationData: PublishSubject<VinylInformation.Data?> = PublishSubject<VinylInformation.Data?>()
     var vinylInformationService: VinylAPIServiceProtocol
@@ -20,20 +22,41 @@ class AddInformationViewModel {
         self.model = AddInformationModel()
         self.vinylInformationService = vinylInformationService
 
-        print("init()", model.vinylID)
 //        _ = self.vinylInformationService.getVinylDetail(vinylID: model.vinylID)
 //            .bind(to: vinylInformationData)
 //            .disposed(by: disposeBag)
     }
 
     func fetchVinylInformation() {
-        print(self.model.vinylID)
+        print("model.viynlID: ",self.model.vinylID)
         
         self.vinylInformationService.requestVinylDetail(vinylID: model.vinylID)
             .do(onNext:{ [weak self] data in
                 self?.vinylInformationDataModel = data
             })
             .bind(to: vinylInformationData)
+            .disposed(by: disposeBag)
+    }
+
+    func deleteVinylBoxData(deleteDispatchGroup: DispatchGroup) {
+        print("deleteVinylBoxData")
+        guard let vinylID = model.vinylID else { return }
+        let vinylDeleteAPI = APITarget.deleteVinyl(vinylID: vinylID)
+
+        CommonNetworkManager.request(apiType: vinylDeleteAPI)
+            .observeOn(ConcurrentMainScheduler.instance)
+            .subscribe(onSuccess: { (response: DeleteVinylResponse) in
+                guard response.data != nil else {
+                    deleteDispatchGroup.leave()
+                    return
+                }
+
+                print(response)
+                CoreDataManager.shared.deleteVinyl(vinylID: Int64(vinylID), dispatchGroup: deleteDispatchGroup)
+            }, onError: { error in
+                print(error)
+                deleteDispatchGroup.leave()
+            })
             .disposed(by: disposeBag)
 
     }
