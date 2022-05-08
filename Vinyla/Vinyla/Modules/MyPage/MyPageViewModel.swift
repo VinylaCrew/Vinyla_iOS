8027ef669291672641225ddd3bc5ab97795b2d91
ijoom <ghfdld@naver.com>
2022-05-08 22:40:50 +0900

    feat: 마케팅 수신 통신 Stream Logic 구현

--- Vinyla/Vinyla/Modules/MyPage/MyPageViewModel.swift
+++ Vinyla/Vinyla/Modules/MyPage/MyPageViewModel.swift
@@ -6,7 +6,43 @@
 //
 
 import Foundation
+import RxSwift
 
 final class MyPageViewModel {
     
+    //input
+    let marketingSubscribed = PublishSubject<Bool>()
+    //output
+    let marketingCompleteSubject = PublishSubject<Bool>()
+    
+    let disposeBag = DisposeBag()
+    
+    init() {
+        
+        self.marketingSubscribed
+            .subscribe(onNext: { [weak self] value in
+                self?.requestMarketingSubscribed(isSubscribeAgreed: value)
+            })
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func clearUserData() {
+        VinylaUserManager.clearAllUserSetting()
+        VinylaUserManager.myVInylIndex = -1
+        CoreDataManager.shared.clearAllObjectEntity("MyImage")
+        CoreDataManager.shared.clearAllObjectEntity("VinylBox")
+    }
+    
+    func requestMarketingSubscribed(isSubscribeAgreed: Bool) {
+        let subscribedData = MarketingSubscribedRequest(subscribeAgreed: isSubscribeAgreed)
+        let marketingAPITarget = APITarget.changeMarketingSubscribed(subscribed: subscribedData)
+        CommonNetworkManager.request(apiType: marketingAPITarget)
+            .subscribe(onSuccess: { (response: VinylaErrorModel) in
+                if response.success {
+                    self.marketingCompleteSubject.onNext(true)
+                }
+            })
+            .disposed(by: self.disposeBag)
+    }
 }
