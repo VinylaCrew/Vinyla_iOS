c9728edeb240b4eafb2bc640349f88ab9a2ff243
ijoom <ghfdld@naver.com>
2022-05-21 23:56:53 +0900

    fix: popup view 목적에 따른 분기처리

--- Vinyla/Vinyla/Modules/AddInformation/POPUPView/FavoriteVinylPOPUPViewController.swift
+++ Vinyla/Vinyla/Modules/AddInformation/POPUPView/FavoriteVinylPOPUPViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol POPUPButtonTapDelegate: AnyObject {
     func didTapFavoriteButton()
+    func didTapDeleteButton()
 }
 
 final class FavoriteVinylPOPUPViewController: UIViewController {
@@ -18,10 +19,13 @@ final class FavoriteVinylPOPUPViewController: UIViewController {
     @IBOutlet weak var registerButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     weak var delegate: POPUPButtonTapDelegate?
+    var mode: String?
     
-    static func initInstance(delegate: POPUPButtonTapDelegate) -> FavoriteVinylPOPUPViewController {
+    static func initInstance(delegate: POPUPButtonTapDelegate, mode: String) -> FavoriteVinylPOPUPViewController {
         let viewController = FavoriteVinylPOPUPViewController(nibName: "FavoriteVinylPOPUPViewController", bundle: Bundle(for: FavoriteVinylPOPUPViewController.self))
         viewController.delegate = delegate
+        viewController.mode = mode
+        print("init")
         return viewController
     }
     
@@ -31,14 +35,24 @@ final class FavoriteVinylPOPUPViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("popupview viewDidLoad")
         self.guideLabel.numberOfLines = 0
-        self.guideLabel.text = "이 바이닐을 대표 바이닐로\n 등록하시겠어요?"
+        
         self.popupView.layer.cornerRadius = 8
         self.popupView.layer.cornerCurve = .continuous
         
         self.registerButton.titleLabel?.font = UIFont(name: "NotoSansKR-Medium", size: 15)
         self.cancelButton.titleLabel?.font = UIFont(name: "NotoSansKR-Medium", size: 15)
+        
+        if self.mode == "vinyl" {
+            self.guideLabel.text = "이 바이닐을 대표 바이닐로\n 등록하시겠어요?"
+            self.registerButton.setTitle("등록", for: .normal)
+            self.registerButton.setTitleColor(UIColor.vinylaMainOrangeColor(), for: .normal)
+        }else if self.mode == "delete" {
+            self.guideLabel.text = "이 바이닐을 정말 삭제하시겠어요?"
+            self.registerButton.setTitle("삭제", for: .normal)
+            self.registerButton.setTitleColor(UIColor.deleteOKPopupColor(), for: .normal)
+        }
 
     }
 
@@ -47,7 +61,12 @@ final class FavoriteVinylPOPUPViewController: UIViewController {
     }
 
     @IBAction func touchUpRegisterButton(_ sender: Any) {
-        self.delegate?.didTapFavoriteButton()
+        if self.mode == "vinyl" {
+            self.delegate?.didTapFavoriteButton()
+        }else if self.mode == "delete" {
+            self.delegate?.didTapDeleteButton()
+        }
+        
         self.dismiss(animated: true, completion: nil)
     }
 }
