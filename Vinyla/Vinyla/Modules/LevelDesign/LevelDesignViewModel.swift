3c69afea9297e7bdb973e34e146d8f488d7ef468
ijoom <ghfdld@naver.com>
2022-05-23 18:32:45 +0900

    fix: 파일 접근자 변경

--- Vinyla/Vinyla/Modules/LevelDesign/LevelDesignViewModel.swift
+++ Vinyla/Vinyla/Modules/LevelDesign/LevelDesignViewModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 final class LevelDesignViewModel {
-    var levelDesignCellID: String = "LevelDesignCell"
+    private var levelDesignCellID: String = "LevelDesignCell"
     var levelDesignModel: [LevelDesignModel] = [LevelDesignModel]()
 
     init() {
