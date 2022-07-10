ac212c1f8ca3b4e9bc5e7caa1ad21004e2f50ec2
ijoom <ghfdld@naver.com>
2022-07-10 23:16:03 +0900

    feat: 문의 메일 보내기 구현

--- Vinyla/Vinyla/Modules/MyPage/MyPageViewController.swift
+++ Vinyla/Vinyla/Modules/MyPage/MyPageViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import MessageUI
+
 import RxSwift
 import RxCocoa
 import Firebase
@@ -18,6 +20,7 @@ final class MyPageViewController: UIViewController {
     @IBOutlet weak var popButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
     @IBOutlet weak var withdrawButton: UIButton!
+    @IBOutlet weak var contactButton: UIButton!
     @IBOutlet weak var marketingSwitch: UISwitch!
     @IBOutlet weak var loginUserLabel: UILabel!
     @IBOutlet weak var loginUserImageView: UIImageView!
@@ -84,6 +87,12 @@ final class MyPageViewController: UIViewController {
                 self?.coordinator?.moveToWithdrawViewController()
             })
             .disposed(by: disposeBag)
+        
+        self.contactButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                self?.sendContactMail()
+            })
+            .disposed(by: disposeBag)
                     
         
         self.serviceInformationButton.rx.tap
@@ -140,5 +149,74 @@ final class MyPageViewController: UIViewController {
     
     @IBAction func touchupPOPButton(_ sender: Any) {
         self.coordinator?.popViewController()
+    }
+    
+    //MARK: - 문의하기 임시 로직 (바닐라 인스타그램 개설 후 변경)
+    func sendContactMail() {
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            
+            let bodyString = """
+                             이곳에 문의 내용을 작성해주세요.
+                             
+                             
+                             -------------------
+                             
+                             Device Model : \(self.getDeviceIdentifier())
+                             Device OS : \(UIDevice.current.systemVersion)
+                             App Version : \(self.getCurrentVersion())
+                             
+                             -------------------
+                             """
+            
+            composeViewController.setToRecipients(["vinylacrew@gmail.com"])
+            composeViewController.setSubject("<Vinyla> 문의 및 의견")
+            composeViewController.setMessageBody(bodyString, isHTML: false)
+            
+            self.present(composeViewController, animated: true, completion: nil)
+        } else {
+            let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패", message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
+            let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+                // 앱스토어로 이동하기(Mail)
+                if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+            let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            
+            sendMailErrorAlert.addAction(goAppStoreAction)
+            sendMailErrorAlert.addAction(cancleAction)
+            self.present(sendMailErrorAlert, animated: true, completion: nil)
+        }
+    }
+    
+    func getDeviceIdentifier() -> String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        
+        return identifier
+    }
+
+    func getCurrentVersion() -> String {
+        guard let dictionary = Bundle.main.infoDictionary,
+              let version = dictionary["CFBundleShortVersionString"] as? String else { return "" }
+        return version
+    }
+    
+}
+
+extension MyPageViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
