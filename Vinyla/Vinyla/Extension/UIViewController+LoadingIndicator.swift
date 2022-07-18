6406636c668da52e5100afb6df0a1425f9dcf796
ijoom <ghfdld@naver.com>
2022-07-18 20:58:20 +0900

    fix: 배경색 수정

--- Vinyla/Vinyla/Extension/UIViewController+LoadingIndicator.swift
+++ Vinyla/Vinyla/Extension/UIViewController+LoadingIndicator.swift
@@ -10,9 +10,9 @@ import UIKit
 fileprivate var loadingGreyView : UIView?
 
 extension UIViewController {
-    func ShowLoadingIndicator() {
+    func showLoadingIndicator() {
         loadingGreyView = UIView(frame: self.view.bounds)
-        loadingGreyView?.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        loadingGreyView?.backgroundColor = UIColor.init(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.4)
         let activityIndicator = UIActivityIndicatorView(style: .large)
         guard let loadingView = loadingGreyView else { return }
         activityIndicator.center = loadingView.center
