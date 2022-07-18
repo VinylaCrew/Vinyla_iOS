f49bd11f4d5500547dbcde3ea9bbce65c51aa52e
ijoom <ghfdld@naver.com>
2022-07-18 20:59:06 +0900

    feat: 비즈니스 로직 VM 수정 및 로딩 인디케이터 추가

--- Vinyla/Vinyla/Modules/AddReview/AddReviewViewModel.swift
+++ Vinyla/Vinyla/Modules/AddReview/AddReviewViewModel.swift
@@ -22,7 +22,9 @@ final class AddReviewViewModel: AddReviewViewModelProtocol {
     var userRate: PublishSubject<Int> = PublishSubject<Int>()
     var userCommnet: PublishSubject<String> = PublishSubject<String>()
     //output
-    var apiError = PublishRelay<NetworkError>()
+    let apiError = PublishRelay<NetworkError>()
+    let isShowLoadingIndicator = PublishRelay<Bool>()
+    let isSavedVinyl = PublishRelay<Bool>()
 
     //model
     var model: RequestSaveVinylModel
@@ -94,7 +96,67 @@ final class AddReviewViewModel: AddReviewViewModelProtocol {
 
             },onError: { [weak self] error in
                 guard let vinylNetworkError = error as? NetworkError else { return }
+                self?.isShowLoadingIndicator.accept(false)
                 self?.apiError.accept(vinylNetworkError)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func requestSaveVinylData2(tthumbailVinylImageData: Data) {
+        if self.model.comment == "이 음반에 대해 감상평을 솔직하게 남겨주세요." {
+            self.model.comment = nil
+        }
+        let chekedCompletedispatchGroup = DispatchGroup()
+        chekedCompletedispatchGroup.enter()
+        
+        chekedCompletedispatchGroup.notify(queue: .main) { [weak self] in
+            
+            self?.isShowLoadingIndicator.accept(false)
+            
+            if CoreDataManager.shared.isSavedSpecificVinyl {
+                self?.isSavedVinyl.accept(true)
+            } else {
+                self?.isSavedVinyl.accept(false)
+            }
+        }
+        
+        let saveViynlAPI = APITarget.saveVinyl(vinylData: self.model)
+        
+        CommonNetworkManager.request(apiType: saveViynlAPI)
+            .subscribe(onSuccess: { [weak self] (response: ResponseSaveVinylModel) in
+                print(response)
+
+                if let vinylIndex = response.data?.vinylIdx {
+                    
+                    if self?.model.rate == 10 {
+                        UIApplication.requestAppStoreReviewIfNeed()
+                    }
+                    
+                    DispatchQueue.global().async() {
+                        
+                        /// 로컬 VinylIndex +1 로직과정
+                        guard let localVinylIndex = VinylaUserManager.userVinylIndex else { return }
+                        VinylaUserManager.userVinylIndex = localVinylIndex + 1
+                        
+                                CoreDataManager.shared.saveVinylBoxWithDispatchGroup(
+                                    uniqueIndex: Int64(vinylIndex),
+                                    vinylIndex: Int64(localVinylIndex + 1),
+                                    vinylID: Int64((self?.model.id)!),
+                                    songTitle: self?.model.title ?? "",
+                                    singer: self?.model.artist ?? "",
+                                    vinylImage: tthumbailVinylImageData,//vinylImage.jpegData(compressionQuality: 1)!,
+                                    dispatchGroup: chekedCompletedispatchGroup
+                                )
+                        
+                    }
+                }
+
+            },onError: { [weak self] error in
+                self?.isShowLoadingIndicator.accept(false)
+                
+                guard let vinylNetworkError = error as? NetworkError else { return }
+                self?.apiError.accept(vinylNetworkError)
+                
             })
             .disposed(by: disposeBag)
     }
